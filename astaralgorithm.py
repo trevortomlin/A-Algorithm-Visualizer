cef18458a41cadce578997c7dbe3e049244f2edb
Trevor <trevortomlin2@outlook.com>
2020-04-07 20:33:49 -0700

    drawing grid and nodes using classes

--- astaralgorithm.py
+++ astaralgorithm.py
@@ -1,14 +1,125 @@
 '''
     File name: astaralgorithm.py
     Author: Trevor Tomlin
-    Date created: 12/23/2019
-    Date last modified: 1/9/2020
+    Date created: 4/7/2020
+    Date last modified: 4/7/2020
     Python Version: 3.7.3
 '''
 
 import pygame
 import math
-import easygui
+
+RED = (255,0,0)
+BLUE = (0,0,255)
+GREEN = (0,255,0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+display_width = 800
+display_height = 800
+menu_bar_size = 300
+
+grid_size = 10
+line_width = 5
+line_color = BLACK
+
+
+class Grid():
+
+	def __init__(self, size=None):
+		self.size = size
+		self.nodes = []
+
+	def draw(self, screen):
+
+		sizeInterval = int(display_height / grid_size)
+
+		for node in self.nodes:
+			node.draw(sizeInterval, screen)
+	
+		for x in range(0, grid_size + 1):
+			pygame.draw.line(screen, line_color, (x * sizeInterval, 0), (x * sizeInterval, display_height), 5)
+		for x in range(0, grid_size + 1 ):
+			pygame.draw.line(screen, line_color, (0 , x * int(display_width / grid_size)), (display_height, x * sizeInterval), 5)
+
+	def button_handler(self, screen, color):
+		mousePos = pygame.mouse.get_pos()
+		col = math.ceil(mousePos[0] / (display_height / grid_size)) - 1
+		row = math.ceil(mousePos[1] / (display_height / grid_size)) - 1
+
+		#print("mouse at", col, row)
+		try:
+			if self.nodes[col + (grid_size * row)].color == WHITE:
+				self.nodes[col + (grid_size * row)].color = color
+
+			elif self.nodes[col + (grid_size * row)].color == color:
+				self.nodes[col + (grid_size * row)].color = WHITE	
+		except:
+  			print("Out of bounds") 
+
+class Node():
+
+	def __init__(self, parent=None, position=None, color=WHITE):
+		self.parent = parent
+		self.position = position
+		self.neighbors = []
+		self.color = color
+
+		self.g = 0
+		self.h = 0
+		self.f = 0
+
+
+	def setF(self):
+		self.f = self.h + self.g
+
+	def setColor(self, color):
+		self.color = color 
+
+	def draw(self, sizeInterval, screen):
+		pygame.draw.rect(screen, self.color, (self.position[0] * sizeInterval, self.position[1] * sizeInterval, display_height / grid_size, display_height / grid_size))
+
+
+
+def main():
+
+	pygame.init()
+	pygame.display.set_caption("A* Algorithm")
+	screen = pygame.display.set_mode((display_width, display_height + menu_bar_size))
+	screen.fill(BLACK)
+	clock = pygame.time.Clock()
+
+	grid = Grid(grid_size)
+	initializeGrid(grid)
+
+	running = True
+
+	while running:
+
+		grid.draw(screen)
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				grid.button_handler(screen, RED)
+
+
+		pygame.display.update()
+		clock.tick(60)
+
+def initializeGrid(grid):
+	for j in range(grid.size):
+		for i in range(grid.size):
+			node = Node(None, (i,j))
+			grid.nodes.append(node)
+			#print(node.position)
+
+
+if __name__ == "__main__":
+	main()
+
+'''
 
 def main():
 
@@ -105,9 +216,9 @@ def main():
 						selectingWall = False
 
 						#Stars the pathfinding algorithm and draws the path
-						path = a_star(tiles, start, end, distSelect)
+						path = a_star(tiles, start, end, distSelect, screen)
 						if path != None:
-							drawPath(path, tiles)
+							drawPath(path, tiles)				
 
 
 		pygame.display.update()
@@ -163,6 +274,8 @@ def button_handler(tiles, size, screen, int):
 	mousePos = pygame.mouse.get_pos()
 	col = math.ceil(mousePos[0] / (screen.get_height() / size)) - 1
 	row = math.ceil(mousePos[1] / (screen.get_height() / size)) - 1
+
+	#print("mouse at", col, row)
 
 	if tiles[row][col] == 0:
 		tiles[row][col] = int
@@ -353,19 +466,38 @@ def find_neighbors(current_Node, tiles):
 	return(neighbors)
 
 #Distance function for algorithm
-def hueristic(current_Node, end_Node, distSelect):
+def heuristic(current_Node, end_Node, distSelect):
 
 	#Euclidean
 	if distSelect:
-		a2 = (current_Node.position[0] - end_Node.position[0]) ** 2
-		b2 = (current_Node.position[1] - end_Node.position[1]) ** 2
-		return int(math.floor(10 * math.sqrt(a2 + b2)))
+		a = (current_Node.position[0] - end_Node.position[0])**2
+		b = (current_Node.position[1] - end_Node.position[1])**2
+		c = int(10 * math.sqrt(a + b))
+		#print(c)
+		return(c)
+		#print("current_Node pos", current_Node.position, "end_Node pos", end_Node.position)
+		#a = (current_Node.position[0] - end_Node.position[0])**2
+		#b = (current_Node.position[1] - end_Node.position[1])**2
+		#c = int(math.floor(10 * math.sqrt(a + b)))
+		#c = int(10 * math.sqrt(a + b))
+		#print(a,b,c)
+		#return c
 
 	#Manhattan
 	elif not distSelect:
-		deltaX = abs(current_Node.position[0] - end_Node.position[0])
-		deltaY = abs(current_Node.position[1] - end_Node.position[1])
-		return 10 * (deltaX + deltaY)
+
+		dstX = abs(current_Node.position[0] - end_Node.position[0])
+		dstY = abs(current_Node.position[1] - end_Node.position[1])
+
+		if (dstX > dstY):
+			return 14*dstY + 10* (dstX-dstY)
+		return 14*dstX + 10 * (dstY-dstX)
+
+
+		#dX = abs(current_Node.position[0] - end_Node.position[0])
+		#dY = abs(current_Node.position[1] - end_Node.position[1])
+		#return 10 * (dX + dY)
+
 
 def a_star(tiles, start, end, distSelect):
 
@@ -379,14 +511,19 @@ def a_star(tiles, start, end, distSelect):
 	end_Node = Node(None, end)
 	end_Node.g = end_Node.h = end_Node.f = 0 
 
-	start_Node.h = hueristic(start_Node, end_Node, distSelect)
+	start_Node.h = heuristic(start_Node, end_Node, distSelect)
 	start_Node.f = start_Node.g + start_Node.h
+	#print(start_Node.f)
 
 	current_Node = start_Node
 
 	openList.append(current_Node)
 
-	while (len(openList) > 0):
+	#while (len(openList) > 0):
+	while end_Node not in closedList:
+
+		if len(openList) == 0:
+			return None
 
 		current_Node = openList[0]
 		current_Index = 0
@@ -397,6 +534,9 @@ def a_star(tiles, start, end, distSelect):
 				current_Index = index
 				current_Node = openList[current_Index]
 
+		openList.remove(current_Node)
+		closedList.append(current_Node)
+
 		#If the algorithm found the end return the path
 		if current_Node == end_Node:
 			path = []
@@ -406,12 +546,9 @@ def a_star(tiles, start, end, distSelect):
 				current = current.parent
 			return path[::-1]
 
-		openList.remove(current_Node)
-		closedList.append(current_Node)
-
 		neighbors = find_neighbors(current_Node, tiles)
 
-		#Determine F scores of neighbors and fidn best path
+		#Determine F scores of neighbors and find best path
 		for neighbor in neighbors:
 
 			if neighbor not in closedList and neighbor not in openList:					
@@ -420,9 +557,108 @@ def a_star(tiles, start, end, distSelect):
 
 					neighbor.parent = current_Node
 
-					neighbor.g = hueristic(neighbor, start_Node, distSelect)
-					neighbor.h = hueristic(neighbor, end_Node, distSelect)
+					neighbor.g = current_Node.g + heuristic(neighbor, current_Node, distSelect)
+					neighbor.h = heuristic(neighbor, end_Node, distSelect)
 					neighbor.f = neighbor.h + neighbor.g
+
+			elif neighbor in openList:
+				#print("neighbor.position: ", neighbor.position)
+				#print("neighbor.g: ", neighbor.g)
+				#print("neighbor heuristic", heuristic(neighbor, start_Node, distSelect))
+				
+				if neighbor.g < (neighbor.parent.g + heuristic(neighbor, neighbor.parent, distSelect)):
+					neighbor.parent = current_Node
+					neighbor.g = neighbor.parent.g + heuristic(neighbor, neighbor.parent, distSelect)
+					neighbor.f = neighbor.h + neighbor.g
+				
+				if neighbor.g < current_Node.g:
+					neighbor.parent = current_Node
+					neighbor.g = current_Node.g + heuristic(neighbor, current_Node, distSelect)
+					neighbor.f = neighbor.h + neighbor.g
+
+			print("pos:", neighbor.position,"parent pos:", neighbor.parent.position,"currentNode g:", current_Node.g,"g:", neighbor.g, "h:", neighbor.h, "f:", neighbor.f, neighbor in openList)
+
+
+		#openList.remove(current_Node)
+		#closedList.append(current_Node)
+
+
+def a_star(tiles, start, end, distSelect, screen):
+
+	start_Node = Node(None, start)
+	end_Node = Node(None, end)
+
+	start_Node.g = 0
+	start_Node.h = heuristic(start_Node, end_Node, distSelect)
+	start_Node.f = start_Node.g + start_Node.h
+
+	end_Node.g = end_Node.h = end_Node.f = 0 
+
+	openSet = []
+	closedSet = []
+
+	openSet.append(start_Node)
+
+	while(len(openSet) > 0):
+
+		node = openSet[0]
+		for index in range(len(openSet)):
+			if (openSet[index].f < node.f or openSet[index].f == node.f):
+				if (openSet[index].h < node.h):
+					node = openSet[index]
+
+		openSet.remove(node)
+		closedSet.append(node)
+
+		if (node == end_Node):
+			path = []
+			current = node
+			while current is not None:
+				path.append(current.position)
+				current = current.parent
+			return path[::-1]
+
+		neighbors = find_neighbors(node, tiles)
+
+		for neighbor in neighbors:
+
+			updatedCost = node.g + heuristic(node, neighbor, distSelect)
+			
+			if(neighbor in closedSet):
+				continue
+
+			elif (updatedCost < neighbor.g):
+				neighbor.g = updatedCost
+				neighbor.h = heuristic(neighbor, end_Node, distSelect) 
+				neighbor.f = neighbor.g + neighbor.h
+				neighbor.parent = current_Node
+
+				if(neighbor not in openSet):
+					openSet.append(neighbor)
+
+			
+			updatedCost = node.g + heuristic(node, neighbor, distSelect)
+
+			if neighbor in closedSet:
+				continue
+
+			if neighbor.g == None:
+				neighbor.g = node.g
+
+
+			elif(updatedCost < neighbor.g or neighbor not in openSet):
+				neighbor.g = updatedCost
+				neighbor.h = heuristic(neighbor, end_Node, distSelect)
+				neighbor.f = neighbor.h + neighbor.g
+				neighbor.parent = node
+
+				if (neighbor not in openSet):
+					openSet.append(neighbor)
+
+			
+
+			print("pos:", neighbor.position,"parent pos:", neighbor.parent.position,"currentNode g:", node.g,"g:", neighbor.g, "h:", neighbor.h, "f:", neighbor.f)
+
 
 #Colors the found path
 def drawPath(path, tiles):
@@ -430,6 +666,6 @@ def drawPath(path, tiles):
 		if tiles[coord[0]][coord[1]] != 1 and tiles[coord[0]][coord[1]] != 2:
 			tiles[coord[0]][coord[1]] = 4
 
-
 if __name__ == "__main__":
 	main()
+'''
